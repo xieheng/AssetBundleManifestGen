59f6c608148f34843d3fa65f05dda5aeeb7271eb
xieheng84 <xieheng84@163.com>
2020-12-10 23:39:36 +0800

    初始文件

---
+++ Assets/AssetBundleManifestGen/Editor/AssetBundleManifestGenWindow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleManifestGenWindow : EditorWindow
+{
+    private AssetBundleManifestGenView view = null;
+
+    [MenuItem("Window/Gen AssetBundle Manifest")]
+    static void Create()
+    {
+        var window = EditorWindow.GetWindow(typeof(AssetBundleManifestGenWindow)) as AssetBundleManifestGenWindow;
+        window.Init();
+        window.Show();
+    }
+
+    void Init()
+    {
+        var model = new AssetBundleManifestGenModel();
+        var ctrl = new AssetBundleManifestGenCtrl(model);
+        view = new AssetBundleManifestGenView(model, ctrl);
+    }
+
+    void OnGUI()
+    {
+        view.OnGUI();
+    }
+}
---
+++ Assets/AssetBundleManifestGen/Editor/Internal/GUI/AssetBundleManifestGenCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AssetBundleManifestGenCtrl
+{
+    public class AB
+    {
+        public string name { get; set; } = string.Empty;
+        public string hash { get; set; } = string.Empty;
+        public Dictionary<string, string> assets { get; } = new Dictionary<string, string>();
+    }
+    
+    private AssetBundleManifestGenModel model = null;
+
+    public AssetBundleManifestGenCtrl(AssetBundleManifestGenModel model)
+    {
+        this.model = model;
+    }
+}
